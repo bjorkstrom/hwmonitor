fbe4284547da3d1d1192acfdd3e3cc2fb789aa49
elmir <elmir@brab>
2017-06-12 10:09:54 +0200

    write reports to a file

--- hwmonitor/Program.cs
+++ hwmonitor/Program.cs
@@ -1,14 +1,27 @@
 using System;
+using System.IO;
 using System.Threading;
 
 
 class Program
 {
+    const string ReportFile = "HardwareMonitorReport.txt";
+
+    static StreamWriter ReportStream;
+
     /* time between we fetch new hardware reports, in miliseconds */
     const int ReportRate = 5000;
 
+    static void OpenReportStream()
+    {
+        var DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string path = Path.Combine(DesktopPath, ReportFile);
+        ReportStream = new StreamWriter(path, true);
+    }
+
     static void Init()
     {
+        OpenReportStream();
         M4ATX.Init();
         Motherboard.Init();
     }
@@ -21,7 +34,14 @@ class Program
         {
             var report = M4ATX.GetReport() + "\n" +
                          Motherboard.GetReport();
+
+            /* write report to console for debugging purpuses */
             Console.WriteLine(report);
+
+            /* write report to the file */
+            ReportStream.WriteLine(report);
+            ReportStream.Flush();
+
             Thread.Sleep(ReportRate);
         }
     }
