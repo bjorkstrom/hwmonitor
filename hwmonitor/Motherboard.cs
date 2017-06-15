e5310cd73f69b6e64ed3b54355873ed5302bf3cb
elmir <elmir@brab>
2017-06-15 10:11:09 +0200

    report CPU and GPU temperature and CPU power
    * adds support for reporting CPU cores and GPU temperature
    * adds suuport for reporting CPU core power usage
    
    Code developed by Moustafa.

--- hwmonitor/Motherboard.cs
+++ hwmonitor/Motherboard.cs
@@ -1,15 +1,58 @@
 using System;
+using OpenHardwareMonitor.Hardware;
+using System.IO;
+using System.Globalization;
+using System.Collections;
 
 class Motherboard
 {
+    public static Computer computer = new Computer();
     public static void Init()
     {
-        /* dummy initialization code */
-        Console.WriteLine("init motherboard reporting");
+        computer.Open();
+        computer.CPUEnabled = true;
+        computer.GPUEnabled = true;
     }
 
     public static string GetReport()
     {
-        return "{Motherboard_Report}";
+        ArrayList list = new ArrayList()
+        {
+            "/intelcpu/0/temperature/0",
+            "/intelcpu/0/temperature/1",
+            "/intelcpu/0/temperature/2",
+            "/intelcpu/0/temperature/3",
+            "/intelcpu/0/temperature/4",
+            "/intelcpu/0/power/0",
+            "/intelcpu/0/power/1",
+            "/intelcpu/0/power/2",
+            "/intelcpu/0/power/3",
+            "/nvidiagpu/0/temperature/0"
+        };
+
+        string s_identifier;
+
+        using (StringWriter w = new StringWriter(CultureInfo.InvariantCulture))
+        {
+            foreach (var hardware in computer.Hardware)
+            {
+                hardware.Update();
+                w.WriteLine();
+                w.WriteLine("Hardware name : " + hardware.Name);
+
+                foreach (var sensor in hardware.Sensors)
+                {
+                    s_identifier = Convert.ToString(sensor.Identifier);
+                    if (list.Contains(s_identifier))
+                    {
+                        w.WriteLine("Sensor name : " + sensor.Name +
+                                    " Sensor type : " + sensor.SensorType +
+                                    " Sensor value : " + sensor.Value);
+                    }
+                }
+            }
+
+            return w.ToString();
+        }
     }
 }
