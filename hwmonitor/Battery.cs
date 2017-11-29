07688186f95cde957a773f2525b72939d9e5bdeb
Katalin <katalinkalman21@gmail.com>
2017-11-29 10:49:35 +0100

    Added a using statement.

--- hwmonitor/Battery.cs
+++ hwmonitor/Battery.cs
@@ -1,41 +1,44 @@
 using System.Diagnostics;
 using System.Linq;
 
-public static class Battery
+namespace hwmonitor
 {
-    static float minVoltage = 10.9F;
-    static int numtimesForAverageVolt = 3;
-    static int NumtimesCheckedVolt { get; set; }
-
-    static float[] voltages = new float[numtimesForAverageVolt];
-    static float[] Voltages { get { return voltages; } }
-
-    ///
-    /// Log that we are shutting down and issue shutdown command
-    ///
-    static void Shutdown(float voltageIn)
+    public static class Battery
     {
-        var msg = string.Format(
-            "Battery voltage {0} below minimal threshold {1}, " +
-            "issuing a shutdown command", voltageIn, minVoltage);
-        Log.WriteLine(msg);
-        Process.Start("shutdown", "-s -t 30");
-    }
+        static float minVoltage = 10.9F;
+        static int numtimesForAverageVolt = 3;
+        static int NumtimesCheckedVolt { get; set; }
 
-    public static void CheckLevel(float voltageIn)
-    {
-        if (NumtimesCheckedVolt != numtimesForAverageVolt)
+        static float[] voltages = new float[numtimesForAverageVolt];
+        static float[] Voltages { get { return voltages; } }
+
+        ///
+        /// Log that we are shutting down and issue shutdown command
+        ///
+        static void Shutdown(float voltageIn)
         {
-            Voltages[NumtimesCheckedVolt] = voltageIn;
-            NumtimesCheckedVolt++;
-            return;
+            var msg = string.Format(
+                "Battery voltage {0} below minimal threshold {1}, " +
+                "issuing a shutdown command", voltageIn, minVoltage);
+            Log.WriteLine(msg);
+            Process.Start("shutdown", "-s -t 30");
         }
 
-        var AverageVolt = Voltages.Sum() / numtimesForAverageVolt;
-        if (AverageVolt < minVoltage)
+        public static void CheckLevel(float voltageIn)
         {
-            Shutdown(voltageIn);
+            if (NumtimesCheckedVolt != numtimesForAverageVolt)
+            {
+                Voltages[NumtimesCheckedVolt] = voltageIn;
+                NumtimesCheckedVolt++;
+                return;
+            }
+
+            var AverageVolt = Voltages.Sum() / numtimesForAverageVolt;
+            if (AverageVolt < minVoltage)
+            {
+                Shutdown(voltageIn);
+            }
+            NumtimesCheckedVolt = 0;
         }
-        NumtimesCheckedVolt = 0;
     }
 }
--- hwmonitor/Program.cs
+++ hwmonitor/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using hwmonitor;
 
 class Program
 {
